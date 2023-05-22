6604cedd4bfd3f10ca811eb9a843d7435ee636ab
osh <dhtpgus7@gmail.com>
2023-05-22 22:06:59 +0900

    next redirects, rewrites

--- next.config.js
+++ next.config.js
@@ -11,6 +11,32 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true,
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/osh',
+        destination: '/about/me/osh',
+      },
+      {
+        source: '/items/:slug',
+        destination: '/products/:slug',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
