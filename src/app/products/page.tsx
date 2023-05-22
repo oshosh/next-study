17adfe09cb31ec14b19f05af9b25ad2253e8b0bb
osh <dhtpgus7@gmail.com>
2023-05-22 22:44:56 +0900

    next push > vercel test

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -19,7 +19,7 @@ async function ProductPage() {
 
   return (
     <div>
-      <h1>제품 소개 페이지!</h1>
+      <h1>제품 소개 페이지!~~~~~</h1>
       <Image src={clothesImage} alt='Clothes' priority />
       <ul>
         {products.map(({ id, name }, index) => {
