9575ec6e475f74a11ea7c9ee57d4dc306fc20cc3
osh <dhtpgus7@gmail.com>
2023-08-14 23:37:14 +0900

    chore: sc 정리

--- src/lib/RootStyleRegistry.tsx
+++ src/lib/RootStyleRegistry.tsx
@@ -2,11 +2,25 @@
 
 import { useServerInsertedHTML } from 'next/navigation';
 import { useStyledComponentsRegistry } from './useStyledComponentsRegistry';
+import { ThemeProvider as theme } from '@/themes';
+import { GlobalStyle } from '@/themes/globalStyles';
+import { ThemeProvider } from 'styled-components';
 
+/**
+ * Next 13 Styled Components Settings
+ * SSR, GlobalStyle, DefaultTheme..
+ */
 export default function RootStyleRegistry({ children }: { children: React.ReactNode }) {
   const [StyledComponentsRegistry, styledComponentsFlushEffect] = useStyledComponentsRegistry();
 
   useServerInsertedHTML(() => <>{styledComponentsFlushEffect()}</>);
 
-  return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
+  return (
+    <StyledComponentsRegistry>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </StyledComponentsRegistry>
+  );
 }
